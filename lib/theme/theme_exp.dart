64c30a40421b24fac38129c4352ab5176b81d107
John <jyaxp1@miumg.edu.gt>
2024-05-19 00:14:53 -0600

    fix o no fix

--- lib/theme/theme_exp.dart
+++ lib/theme/theme_exp.dart
@@ -1,4 +1,4 @@
-export 'package:e_proyecto/theme/elevatedButton.dart';
+export 'package:e_proyecto/theme/elevatedbutton.dart';
 export 'package:e_proyecto/theme/text_theme.dart';
 export 'package:e_proyecto/theme/appbar_theme.dart';
 export 'package:e_proyecto/theme/checkbox_theme.dart';
@@ -6,5 +6,4 @@ export 'package:e_proyecto/theme/chip_theme.dart';
 export 'package:e_proyecto/theme/bottonsheet_theme.dart';
 export 'package:e_proyecto/theme/outlinedbutton_theme.dart';
 export 'package:e_proyecto/theme/textfield_theme.dart';
-
 export 'package:e_proyecto/theme/theme.dart';
