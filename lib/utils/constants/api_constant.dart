dfa16f2cec4a33c1a65343928b74c8d69f8ffc1b
John <jyaxp1@miumg.edu.gt>
2024-05-20 23:29:35 -0600

    Se añadieron assets y modificacon a utils

--- lib/utils/constants/api_constant.dart
+++ lib/utils/constants/api_constant.dart
@@ -1,1 +1,2 @@
 //lista de api y apikey
+//const String tSecretAPIKey = "cwt_live_b2da6ds3df3e785v8ddc59198f7615ba";
--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -1,60 +1,47 @@
 import 'package:flutter/material.dart';
 
-class TColors{
-  TColors._();
+class TColors {
+  // App theme colors
+  static const Color primary = Color(0xFF4b68ff);
+  static const Color secondary = Color(0xFFFFE24B);
+  static const Color accent = Color(0xFFb0c7ff);
 
-  //colores app
-  static const Color primary = Color.fromARGB(255, 200, 106, 219);
-  static const Color secondary = Color.fromARGB(255, 214, 224, 64);
-  static const Color accent = Color.fromARGB(255, 213, 181, 218);
+  // Text colors
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color textSecondary = Color(0xFF6C757D);
+  static const Color textWhite = Colors.white;
 
-  //Gradient colors
-  static const Gradient linerGradient = LinearGradient(
-    begin: Alignment(0.0, 0.0), 
-    end: Alignment(0.707, -0.707),
-    colors: [
-      Color.fromARGB(255, 200, 106, 219),
-      Color. fromARGB(255, 200, 80, 190),
-      Color.fromARGB(255, 214, 60, 120),
-      ],
-  );
+  // Background colors
+  static const Color light = Color(0xFFF6F6F6);
+  static const Color dark = Color(0xFF272727);
+  static const Color primaryBackground = Color(0xFFF3F5FF);
 
-  //Colores de texto
-  static const Color textPrimary = Color.fromARGB(255, 0, 0, 0);
-  static const Color textsecondeary = Color.fromARGB(255, 87, 86, 86);
-  static const Color textwhite = Color.fromARGB(255, 255, 255, 255);
+  // Background Container colors
+  static const Color lightContainer = Color(0xFFF6F6F6);
+  static Color darkContainer = TColors.white.withOpacity(0.1);
 
-  //Colores de fondo
-  static const Color light = Color.fromARGB(255, 255, 255, 255);
-  static const Color dark = Color.fromARGB(255, 51, 50, 50);
-  static const Color primarybackground = Color.fromARGB(255, 255, 255, 255);
+  // Button colors
+  static const Color buttonPrimary = Color(0xFF4b68ff);
+  static const Color buttonSecondary = Color(0xFF6C757D);
+  static const Color buttonDisabled = Color(0xFFC4C4C4);
 
-  //background container color
-  static const Color lightcontainer = Color.fromARGB(255, 255, 255, 255);
-  static const Color darkcontainer = Color.fromARGB(255, 51, 50, 50);
+  // Border colors
+  static const Color borderPrimary = Color(0xFFD9D9D9);
+  static const Color borderSecondary = Color(0xFFE6E6E6);
 
-  //button colors
-  static const Color primaryButton = Color.fromARGB(255, 200, 106, 219);
-  static const Color secondaryButton = Color.fromARGB(255, 213, 181, 218);
-  static const Color disabledButton = Color.fromARGB(255, 143, 143, 141);
+  // Error and validation colors
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+  static const Color warning = Color(0xFFF57C00);
+  static const Color info = Color(0xFF1976D2);
 
-  //border colors
-  static const Color borderPrimary = Color.fromARGB(255, 143, 143, 141);
-  static const Color borderSecondary = Color.fromARGB(255, 255, 255, 255);
-
-  //error and validation colors
-  static const Color error = Color.fromARGB(255, 255, 0, 0);
-  static const Color success = Color.fromARGB(255, 65, 165, 70);
-  static const Color warning = Color.fromARGB(255, 255, 165, 0);
-  static const Color info = Color.fromARGB(255, 0, 0, 255);
-
-  //Neutral shades
-  static const Color neutral1 = Color.fromARGB(255, 51, 51, 51);
-  static const Color neutral2 = Color.fromARGB(255, 102, 102, 102);
-  static const Color neutral3 = Color.fromARGB(255, 153, 153, 153);
-  static const Color neutral4 = Color.fromARGB(255, 204, 204, 204);
-  static const Color neutral5 = Color.fromARGB(255, 229, 229, 229);
-  static const Color neutral6 = Color.fromARGB(255, 242, 242, 242);
-  static const Color neutral7 = Color.fromARGB(255, 255, 255, 255);
+  // Neutral Shades
+  static const Color black = Color(0xFF232323);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color white = Color(0xFFFFFFFF);
 
 }
